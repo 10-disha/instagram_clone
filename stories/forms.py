e2faf1ad71e1f45f4c38d9ef2c9486d56dac6a67
yash-2115 <yashp63544@gmail.com>
2021-03-01 14:08:49 +0530

    bug fixed for nidifaction badge and newpost, newstory form design improved

--- stories/forms.py
+++ stories/forms.py
@@ -4,10 +4,13 @@ from stories.models import Story
 
 from django.forms import ClearableFileInput
 
-class NewStoryForm(forms.ModelForm):
-	content = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), required=True)
-	caption = forms.CharField(widget=forms.Textarea(attrs={'class': 'input is-medium'}), required=True)
 
-	class Meta:
-		model = Story
-		fields = ('content', 'caption')
+class NewStoryForm(forms.ModelForm):
+    content = forms.FileField(widget=forms.ClearableFileInput(
+        attrs={'multiple': True, 'class': 'form-control', 'placeholder': 'Please select your file'}), required=True)
+    caption = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'input is-medium'}), required=True)
+
+    class Meta:
+        model = Story
+        fields = ('content', 'caption')
